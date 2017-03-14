04c67d1cfb6f9c849fb7319715f27b4eb750a858
Ashton Kinslow <Ashtonian@users.noreply.github.com>
2017-03-14 17:27:29 -0500

    updating text blurbs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $(".headline").typed({
     strings: [
       "I'm a developer",
-      "I do web stuff",
-      "C# is my bread and butter",
-      "I use sql, a lot",
-      "I play around with node",
-      "I'm learning AngularJS + Ionic this year",
+      "I do backend stuff",
+      "I love Node.js", 
+      "I'm learning React and Meteor",
       "I create things on the internet",
       "I'm a developer",
+      "C# is my bread and butter",
+      "I use sql, a lot"
     ],
     typeSpeed: 35,
     startDelay: 50,
