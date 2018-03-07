3287c8269815527bb379ba6edfa21262639fa1d3
Ashton Kinslow <Ashtonian@users.noreply.github.com>
2018-03-07 16:03:50 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
   $(".headline").typed({
     strings: [
       "I'm a developer",
-      "I mainly do backend stuff",
-      "I love Node.js", 
-      "I'm learning React and Meteor",
-      "Go is my bread and butter",
-      "I use sql, a lot",
+      "I'm a gopher!", 
+      "I play with lots of data",
       "I create things on the internet",
       "I'm a developer"
     ],
