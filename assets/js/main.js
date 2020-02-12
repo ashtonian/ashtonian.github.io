f575ee5b8e18ecc62b1a42cb7515605737d14160
ashtonian <github@ashtonkinslow.com>
2020-02-12 16:52:13 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $(".headline").typed({
     strings: [
       "I'm a developer",
-      "I'm a gopher!", 
+      "I'm a team lead",
       "I play with lots of data",
+      "I'm a goofy gopher!",
       "I create things on the internet",
       "I'm a developer"
     ],
