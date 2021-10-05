afa3805a2f71a654ad03aea28d9021add3b3d7ad
Ashton Kinslow <github@ashtonkinslow.com>
2021-10-05 13:47:47 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $(".headline").typed({
     strings: [
-      "I'm a developer",
-      "I'm a team lead",
+      "I'm a Software Engineer",
+      "I'm a Team Lead",
       "I play with lots of data",
-      "I'm a goofy gopher!",
+      "I love golang!",
       "I create things on the internet",
-      "I'm a developer"
+      "I'm a Software Engineer"
     ],
     typeSpeed: 35,
     startDelay: 50,
