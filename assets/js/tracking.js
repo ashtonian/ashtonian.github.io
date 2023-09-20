66842901b17db4c513e0f42edfd81fe3aa93bfc7
Ashton Kinslow <github@ashtonkinslow.com>
2023-09-20 17:37:17 -0500

    Update tracking.js

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -3,7 +3,7 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//tics.ashlab.dev/";
+  var u="https://tics.ashlab.dev/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '2']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
